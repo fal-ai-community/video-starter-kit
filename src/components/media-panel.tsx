0d06e94be09dba7e8dec6b17c64954d6ef0780fc
Aykut Kardaş <aykutkrds@gmail.com>
2025-01-22 21:45:01 +0300

    lint

--- src/components/media-panel.tsx
+++ src/components/media-panel.tsx
@@ -129,7 +129,7 @@ export function MediaItemRow({
     <div
       className={cn(
         "flex items-start space-x-2 py-2 w-full px-4 hover:bg-accent transition-all",
-        className
+        className,
       )}
       {...props}
       onClick={(e) => {
@@ -145,7 +145,7 @@ export function MediaItemRow({
             "flex items-center h-full cursor-grab text-muted-foreground",
             {
               "text-muted": data.status !== "completed",
-            }
+            },
           )}
         >
           <GripVerticalIcon className="w-4 h-4" />
@@ -249,7 +249,7 @@ export function MediaItemPanel({
     <div
       className={cn(
         "flex flex-col overflow-hidden divide-y divide-border",
-        className
+        className,
       )}
     >
       {data
